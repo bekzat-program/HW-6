f0402d7504191070fdf805054e0c340c253c4479
bekzat-program <23241260@turan-edu.kz>
2024-10-26 12:23:41 +0500

    Create StorageDevice.cs

---
+++ StorageDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW6
+{
+    abstract class StorageDevice
+    {
+        // Поле для ёмкости устройства (в гигабайтах)
+        public int Capacity { get; set; }
+
+        // Абстрактный метод для сохранения данных
+        public abstract void StoreData();
+    }
+}
