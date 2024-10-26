822203f692394b9c116ce17e190057b5340accb0
bekzat-program <23241260@turan-edu.kz>
2024-10-26 12:21:41 +0500

    Create HardDrive.cs

---
+++ HardDrive.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW6
+{
+    class HardDrive : StorageDevice
+    {
+        // Поле для скорости чтения/записи (в мегабайтах в секунду)
+        public int ReadWriteSpeed { get; set; }
+
+        // Реализация метода StoreData для жёсткого диска
+        public override void StoreData()
+        {
+            // Выводим сообщение о сохранении данных с указанием скорости записи
+            Console.WriteLine($"Сохранение данных на жёсткий диск со скоростью {ReadWriteSpeed} МБ/с.");
+        }
+    }
+}
