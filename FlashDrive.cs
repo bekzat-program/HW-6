f6c6000b2533ddeb0de0e8a18b5d567379a2f206
bekzat-program <23241260@turan-edu.kz>
2024-10-26 12:20:47 +0500

    Create FlashDrive.cs

---
+++ FlashDrive.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HW6
+{
+    // Класс для флеш-накопителя, от StorageDevice
+    class FlashDrive : StorageDevice
+    {
+        // Поле для типа USB (например, "USB 2.0" или "USB 3.0")
+        public string UsbType { get; set; }
+
+        // Реализация метода StoreData для флеш-накопителя
+        public override void StoreData()
+        {
+            // Выводим сообщение о сохранении данных с указанием типа USB
+            Console.WriteLine($"Сохранение данных на флеш-накопитель с типом USB: {UsbType}.");
+        }
+    }
+}
